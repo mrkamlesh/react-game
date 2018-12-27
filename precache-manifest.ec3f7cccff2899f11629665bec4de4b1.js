6ebe61f7693d58d64ca31f49b93e58031c4ba960
kamlesh.kumar <kamlesh.kumar@agreeya.com>
2018-12-27 16:40:53 +0530

    Updates

---
+++ precache-manifest.ec3f7cccff2899f11629665bec4de4b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda9c7c5162c773fb401",
+    "url": "/react-game/static/js/runtime~main.cda9c7c5.js"
+  },
+  {
+    "revision": "6683eec578aadf6267f0",
+    "url": "/react-game/static/js/main.6683eec5.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/react-game/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "6683eec578aadf6267f0",
+    "url": "/react-game/static/css/main.085fe573.chunk.css"
+  },
+  {
+    "revision": "11155c636dc09f81e983d30c72a8f387",
+    "url": "/react-game/index.html"
+  }
+];
